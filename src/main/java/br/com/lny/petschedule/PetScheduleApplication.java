8c1065ee5e612fff9a4056a67f299f0ce7ae1769
Leo Yassuda <leo.yassuda@gmail.com>
2020-08-13 21:10:36 -0300

    first commit

---
+++ src/main/java/br/com/lny/petschedule/PetScheduleApplication.java
@@ -0,0 +1,13 @@
+package br.com.lny.petschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetScheduleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lny/petschedule/PetScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lny.petschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
